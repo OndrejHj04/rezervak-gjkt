9fe98622724b4b62a84b18b56036484c2759a9a3
OndrejHj04 <ondra.hajku@seznam.cz>
2023-08-22 20:22:56 +0200

    calendar

--- app/page.tsx
+++ app/page.tsx
@@ -1,37 +1,11 @@
-import { getServerSession } from "next-auth";
-import { getProviders, signIn, useSession } from "next-auth/react";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
-import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
-import { Sign } from "crypto";
-import SigninButton from "@/components/SigninButton";
+import Calendar from "@/components/Calendar";
+import Navbar from "@/components/Navbar";
 
 export default async function Home() {
   return (
     <>
-      <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static">
-          <Toolbar>
-            <IconButton
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="menu"
-              sx={{ mr: 2 }}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              Rezervační systém
-            </Typography>
-            <SigninButton />
-          </Toolbar>
-        </AppBar>
-      </Box>
+      <Navbar />
+      <Calendar />
     </>
   );
 }
---
+++ components/Navbar.tsx
@@ -0,0 +1,35 @@
+import { getServerSession } from "next-auth";
+import { getProviders, signIn, useSession } from "next-auth/react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import { Sign } from "crypto";
+import SigninButton from "@/components/SigninButton";
+
+export default function Navbar() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Rezervační systém
+          </Typography>
+          <SigninButton />
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}

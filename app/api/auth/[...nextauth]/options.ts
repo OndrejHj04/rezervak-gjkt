41c2b9d48c81ffa4692b77d779794923d9793201
OndrejHj04 <ondra.hajku@seznam.cz>
2023-08-23 15:43:20 +0200

    deploy

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -10,9 +10,8 @@ const authOptions = {
   },
   providers: [
     GoogleProvider({
-      clientId:
-        "353469902540-9jltm2hc9q345pmalbrhud8daujo305n.apps.googleusercontent.com",
-      clientSecret: "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",
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
   ],
   callbacks: {

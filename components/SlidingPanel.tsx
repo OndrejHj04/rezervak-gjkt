19663bbdca448d13b1388a0a4223a25853754f34
OndrejHj04 <ondra.hajku@seznam.cz>
2023-08-23 08:43:23 +0200

    panel items

--- components/SlidingPanel.tsx
+++ components/SlidingPanel.tsx
@@ -4,6 +4,7 @@ import {
   Button,
   FormControlLabel,
   Modal,
+  Slide,
   Switch,
   Typography,
 } from "@mui/material";
@@ -14,54 +15,52 @@ import MenuList from "@mui/material/MenuList";
 import MenuItem from "@mui/material/MenuItem";
 import ListItemText from "@mui/material/ListItemText";
 import ListItemIcon from "@mui/material/ListItemIcon";
-import EventIcon from "@mui/icons-material/Event";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import DarkModeIcon from "@mui/icons-material/DarkMode";
 import LogoutIcon from "@mui/icons-material/Logout";
 import { store } from "@/store/store";
+import { signOut } from "next-auth/react";
 
 export default function SlidingPanel() {
   const { panel, setPanel, darkMode, toggleDarkMode } = store();
   console.log(darkMode);
   return (
-    <div>
-      <Modal
-        open={panel}
-        onClose={() => setPanel(false)}
-        className="outline-none"
-      >
-        <Paper className="absolute h-full border-0 outline-none flex flex-col justify-between">
-          <MenuList>
-            <MenuItem className="flex gap-2">
-              <ListItemIcon>
-                <EventIcon fontSize="large" />
-              </ListItemIcon>
-              <ListItemText>
-                <Typography variant="h6">Moje rezervace</Typography>
-              </ListItemText>
-            </MenuItem>
-            <MenuItem className="flex gap-2" onClick={toggleDarkMode}>
-              <ListItemIcon>
-                <DarkModeIcon fontSize="large" />
-              </ListItemIcon>
-              <ListItemText>
-                <Typography variant="h6">Tmavý mód</Typography>
-              </ListItemText>
-              <Switch checked={darkMode} />
-            </MenuItem>
-          </MenuList>
-
-          <MenuList>
-            <MenuItem className="flex gap-2">
-              <ListItemIcon>
-                <LogoutIcon fontSize="large" />
-              </ListItemIcon>
-              <ListItemText>
-                <Typography variant="h6">Odhlásit se</Typography>
-              </ListItemText>
-            </MenuItem>
-          </MenuList>
-        </Paper>
-      </Modal>
-    </div>
+    <Modal
+      open={panel}
+      onClose={() => setPanel(false)}
+      className="outline-none"
+    >
+      <Paper className="absolute h-full border-0 outline-none flex flex-col justify-between">
+        <MenuList>
+          <MenuItem className="flex gap-2">
+            <ListItemIcon>
+              <AccountCircleIcon fontSize="large" />
+            </ListItemIcon>
+            <ListItemText>
+              <Typography variant="h6">Můj profil</Typography>
+            </ListItemText>
+          </MenuItem>
+          <MenuItem className="flex gap-2" onClick={toggleDarkMode}>
+            <ListItemIcon>
+              <DarkModeIcon fontSize="large" />
+            </ListItemIcon>
+            <ListItemText>
+              <Typography variant="h6">Tmavý mód</Typography>
+            </ListItemText>
+            <Switch checked={darkMode} />
+          </MenuItem>
+        </MenuList>
+        <MenuList>
+          <MenuItem className="flex gap-2" onClick={() => signOut()}>
+            <ListItemIcon>
+              <LogoutIcon fontSize="large" color="error" />
+            </ListItemIcon>
+            <ListItemText>
+              <Typography variant="h6">Odhlásit se</Typography>
+            </ListItemText>
+          </MenuItem>
+        </MenuList>
+      </Paper>
+    </Modal>
   );
 }

f9427e980990101472af7776e0eb52e777be82b5
OndrejHj04 <ondra.hajku@seznam.cz>
2023-08-24 17:23:16 +0200

    email param

--- app/api/changetheme/route.ts
+++ app/api/changetheme/route.ts
@@ -1,10 +1,13 @@
 import { NextResponse } from "next/server";
 import { query } from "@/lib/db";
+import { parse } from "url";
 
-export async function POST() {
+export async function POST(req: Request) {
+  const URL = parse(req.url, true);
+  const email = URL.query.email;
   const user = await query({
-    query: `UPDATE users SET theme = "light" WHERE email = ?`,
-    values: ["email@email.com"],
+    query: `UPDATE users SET theme = "dark" WHERE email = ?`,
+    values: [email],
   });
 
   return NextResponse.json({ users: user });

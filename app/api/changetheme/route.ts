826cec3472c66ab977206555e2478f62ef84bafe
OndrejHj04 <ondra.hajku@seznam.cz>
2023-08-25 15:43:39 +0200

    sql update

--- app/api/changetheme/route.ts
+++ app/api/changetheme/route.ts
@@ -6,7 +6,15 @@ export async function POST(req: Request) {
   const URL = parse(req.url, true);
   const email = URL.query.email;
   const user = await query({
-    query: `UPDATE users SET theme = "dark" WHERE email = ?`,
+    query: `
+    UPDATE users
+SET theme = CASE
+    WHEN theme = 'dark' THEN 'light'
+    WHEN theme = 'light' THEN 'dark'
+    ELSE theme
+END
+WHERE email = ?;
+`,
     values: [email],
   });
 

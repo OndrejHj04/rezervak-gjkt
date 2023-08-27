bbb26652a0f94b4e08268482413a5b365ac6d5b2
OndrejHj04 <ondra.hajku@seznam.cz>
2023-08-27 09:54:27 +0200

    avatar ui

--- app/profile/[id]/page.tsx
+++ app/profile/[id]/page.tsx
@@ -1,4 +1,4 @@
-import { Typography } from "@mui/material";
+import { Avatar, Typography } from "@mui/material";
 import { usePathname } from "next/navigation";
 
 const getUserData = async (id) => {
@@ -15,5 +15,10 @@ export default async function Profile({ params: { id } }) {
   if (!data) {
     return <Typography variant="h4">User not found</Typography>;
   }
-  return <Typography variant="h4">Jméno: {data.full_name}</Typography>;
+  return (
+    <div className="p-5 flex items-center gap-2">
+      <Avatar sx={{ width: 60, height: 60 }} />
+      <Typography variant="h4">Jméno: {data.full_name}</Typography>
+    </div>
+  );
 }

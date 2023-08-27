73c4aa017ebd365aaac853f1c078f79e26b3e495
OndrejHj04 <ondra.hajku@seznam.cz>
2023-08-27 11:10:58 +0200

    change photo ui

--- app/profile/[id]/page.tsx
+++ app/profile/[id]/page.tsx
@@ -1,5 +1,6 @@
-import { Avatar, Typography } from "@mui/material";
+import { Avatar, Badge, IconButton, Tooltip, Typography } from "@mui/material";
 import { usePathname } from "next/navigation";
+import ChangeIcon from "@/components/ChangeIcon";
 
 const getUserData = async (id) => {
   const req = await fetch(`http://localhost:3000/api/getuserdata?id=${id}`, {
@@ -18,7 +19,7 @@ export default async function Profile({ params: { id } }) {
   console.log(data);
   return (
     <div className="p-5 flex items-center gap-2">
-      <Avatar sx={{ width: 60, height: 60 }} src={data.photo} />
+      <ChangeIcon photo={data.photo} />
       <Typography variant="h4">Jméno: {data.full_name}</Typography>
     </div>
   );
---
+++ components/ChangeIcon.tsx
@@ -0,0 +1,25 @@
+"use client";
+import { Avatar, IconButton } from "@mui/material";
+import { useState } from "react";
+import ModeIcon from "@mui/icons-material/Mode";
+
+export default function ChangeIcon({ photo }) {
+  const [hover, setHover] = useState(false);
+  return (
+    <div
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+      className="relative"
+    >
+      <Avatar className={`w-16 h-16 ${hover && "opacity-50"}`} src={photo} />
+      <IconButton
+        className={`w-16 h-16 z-30 absolute top-0 opacity-0 transition-all  ${
+          hover && "opacity-100"
+        }`}
+        color="info"
+      >
+        <ModeIcon sx={{ width: 40, height: 40 }} />
+      </IconButton>
+    </div>
+  );
+}

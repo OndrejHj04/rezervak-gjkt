73a45a9d4ce79987ac0230f07065e418634dd161
OndrejHj04 <ondra.hajku@seznam.cz>
2023-09-17 12:05:28 +0200

    project tailwind mui init

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import { Typography } from "@mui/material";
+import { Button, Typography } from "@mui/material";
 
 export default async function Home() {
   return (
     <>
       <Typography>Home</Typography>
+      <Button variant="contained">ahoij</Button>
     </>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,24 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
+  corePlugins: {
+    // Remove the Tailwind CSS preflight styles so it can use Material UI's preflight instead (CssBaseline).
+    preflight: false,
+  },
   plugins: [],
-}
-export default config
+};
+export default config;

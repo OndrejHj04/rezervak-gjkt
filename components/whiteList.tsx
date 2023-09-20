4a01973d6262adadadb8ab4ffc2c4477bbee0706
OndrejHj04 <ondra.hajku@seznam.cz>
2023-09-20 18:12:04 +0200

    user small card

--- components/whiteList.tsx
+++ components/whiteList.tsx
@@ -1,10 +1,33 @@
-import { Divider, Paper, Typography } from "@mui/material";
+import {
+  Avatar,
+  Card,
+  CardHeader,
+  Divider,
+  IconButton,
+  Paper,
+  Slide,
+  Typography,
+} from "@mui/material";
+import UserSmallCard from "@/sub-components/UserSmallCard";
 
-export default function WhiteList() {
+const getUsers = async () => {
+  const response = await fetch(
+    process.env.NEXT_PUBLIC_API_URL + "/api/users/list"
+  );
+  const data = await response.json();
+  return data;
+};
+
+export default async function WhiteList() {
+  const { data } = await getUsers();
+
   return (
-    <Paper className="flex flex-col p-2 h-min">
+    <Paper className="flex flex-col p-2 gap-2 h-min">
       <Typography variant="h5">Seznam uživatelů</Typography>
       <Divider />
+      {data.map((user) => (
+        <UserSmallCard user={user} key={user.id} />
+      ))}
     </Paper>
   );
 }
---
+++ sub-components/UserSmallCard.tsx
@@ -0,0 +1,59 @@
+"use client";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import {
+  Avatar,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  Collapse,
+  IconButton,
+  IconButtonProps,
+  Typography,
+  styled,
+} from "@mui/material";
+import { useEffect, useRef, useState } from "react";
+
+interface ExpandMoreProps extends IconButtonProps {
+  expand: boolean;
+}
+
+const ExpandMore = styled((props: ExpandMoreProps) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
+  marginLeft: "auto",
+  transition: theme.transitions.create("transform", {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
+
+export default function UserSmallCard({ user }) {
+  const [expanded, setExpanded] = useState(false);
+
+  return (
+    <Card key={user.id}>
+      <CardHeader
+        avatar={<Avatar />}
+        action={
+          <ExpandMore
+            expand={expanded}
+            onClick={() => setExpanded((c) => !c)}
+            aria-expanded={expanded}
+            aria-label="show more"
+          >
+            <ExpandMoreIcon />
+          </ExpandMore>
+        }
+        title={<Typography>{user.username}</Typography>}
+        subheader={<Typography variant="body2">{user.email}</Typography>}
+      ></CardHeader>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <Typography variant="body2">#{user.id}</Typography>
+        </CardContent>
+      </Collapse>
+    </Card>
+  );
+}

a1fa41ec9c0695ff73c878eaefbe03428ed11796
OndrejHj04 <ondra.hajku@seznam.cz>
2023-09-23 09:52:29 +0200

    user endpoint with roles

--- app/api/users/list/route.ts
+++ app/api/users/list/route.ts
@@ -6,24 +6,15 @@ export async function GET(req: Request) {
     const url = new URL(req.url);
     const roles = url.searchParams.get("roles")?.split(",");
     const email = url.searchParams.get("email");
-    const placeholders = roles?.map((role) => "?").join(", ") || 0;
-
-    let sql = "SELECT * FROM users";
-    let values = [];
-    if (roles) {
-      sql = `SELECT * FROM users ${
-        placeholders ? `WHERE role IN (${placeholders})` : ""
-      }`;
-      values = roles;
-    } else if (email) {
-      sql = `SELECT * FROM users WHERE email = ?`;
-      values = [email];
-    }
-
     const data = await query({
-      query: sql,
-      values: values,
+      query: `SELECT u.id, u.username, u.email, JSON_OBJECT('role_id', r.id, 'role_name', r.role_name, 'role_color', r.role_color) AS role FROM users u JOIN roles r ON u.role = r.id ${
+        roles || email
+          ? `${roles ? `WHERE r.id = ${roles}` : `WHERE u.email = "${email}"`}`
+          : ""
+      }`,
+      values: [],
     });
+    data.map((item) => (item.role = JSON.parse(item.role as any)));
     return NextResponse.json({
       success: true,
       message: "Operation successful",

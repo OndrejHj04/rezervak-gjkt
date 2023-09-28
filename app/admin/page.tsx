a5d9a8a7c23c8a4e33b60f9a325cafaa55b8f119
OndrejHj04 <ondra.hajku@seznam.cz>
2023-09-28 11:29:43 +0200

    unused components 🚮

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,11 +1,11 @@
 import AdminCredentials from "@/components/adminCredentials";
 import RolesComponent from "@/components/rolesComponent";
-import WhiteList from "@/components/whiteList";
 import { AdminCredentialsType } from "@/models/User";
 
 async function getAdminCredentials() {
   const res = await fetch(
-    process.env.NEXT_PUBLIC_API_URL + "/api/admin/credentials");
+    process.env.NEXT_PUBLIC_API_URL + "/api/admin/credentials"
+  );
   const { data } = await res.json();
   return data[0] as AdminCredentialsType; //attention
 }
@@ -16,8 +16,7 @@ export default async function Admin() {
   return (
     <div className="flex gap-2">
       <AdminCredentials data={data} />
-      <WhiteList />
-      <RolesComponent />
+      {/* <RolesComponent /> */}
     </div>
   );
 }

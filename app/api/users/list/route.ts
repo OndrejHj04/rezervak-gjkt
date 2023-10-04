594459b1bac5da86862e0ea0e731a298032d1699
OndrejHj04 <ondra.hajku@seznam.cz>
2023-10-04 20:23:38 +0200

    make log 🪵

--- app/api/users/list/route.ts
+++ app/api/users/list/route.ts
@@ -11,6 +11,8 @@ export async function GET(req: Request) {
     const url = new URL(req.url);
     const roles = url.searchParams.get("roles")?.split(",");
     const email = url.searchParams.get("email");
+
+    console.log("before query");
     const data = (await query({
       query: `SELECT u.*, JSON_OBJECT('role_id', r.id, 'role_name', r.role_name, 'role_color', r.role_color, 'icon',  r.icon) AS role FROM users u JOIN roles r ON u.role = r.id ${
         roles || email
@@ -19,6 +21,9 @@ export async function GET(req: Request) {
       }`,
       values: [],
     })) as User[];
+
+    console.log("after query", data);
+
     data.map((item) => {
       item.role = JSON.parse(item.role as any);
       item.full_name = `${item.first_name} ${item.last_name}`;
@@ -34,6 +39,7 @@ export async function GET(req: Request) {
       {
         success: false,
         message: "Something went wrong",
+        error: e,
       },
       { status: 500 }
     );

a66a6d19fa780035311e0681e1d2ad76c4b5faa9
OndrejHj04 <ondra.hajku@seznam.cz>
2023-10-05 06:43:35 +0200

    fix

--- app/api/users/get/route.ts
+++ app/api/users/get/route.ts
@@ -2,6 +2,9 @@ import { query } from "@/lib/db";
 import { NextResponse } from "next/server";
 
 export async function GET(req: Request) {
+  const url = new URL(req.url);
+  const roles = url.searchParams.get("roles")?.split(",");
+  const email = url.searchParams.get("email");
   try {
     const data = (await query({
       query: `SELECT u.*, CONCAT('{', 
@@ -14,6 +17,12 @@ export async function GET(req: Request) {
     JOIN roles r ON u.role = r.id`,
       values: [],
     })) as any;
+
+    data.map((item: any) => {
+      item.role = JSON.parse(item.role as any);
+      item.full_name = `${item.first_name} ${item.last_name}`;
+      return item;
+    });
 
     return NextResponse.json({
       success: true,

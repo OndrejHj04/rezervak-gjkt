e48fe54b94dd0b89d536f3de35a7c085a77f78e0
OndrejHj04 <ondra.hajku@seznam.cz>
2023-10-09 16:33:02 +0200

    test

--- sub-components/AddGroupModal.tsx
+++ sub-components/AddGroupModal.tsx
@@ -43,7 +43,7 @@ export default function AddGroupModal() {
     formState: { errors },
   } = useForm<createGroup>();
   const [accounts, setAccounts] = useState<User[] | null>(null);
-
+  console.log("test");
   useEffect(() => {
     fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/users/list?roles=1,2,3`)
       .then((res) => res.json())
@@ -53,7 +53,6 @@ export default function AddGroupModal() {
   const close = () => setModal("");
 
   const onSubmit = (data: any) => {
-
     fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/group/create`, {
       method: "POST",
       body: JSON.stringify(data),

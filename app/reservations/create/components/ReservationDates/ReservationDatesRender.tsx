d370ef3e48fe17d7d60cb322bcb4bbb52e2e0263
OndrejHj04 <ondra.hajku@seznam.cz>
2023-10-22 11:58:23 +0200

    hightlight reservation days

--- app/reservations/create/components/ReservationDates/ReservationDatesRender.tsx
+++ app/reservations/create/components/ReservationDates/ReservationDatesRender.tsx
@@ -3,24 +3,60 @@ import {
   Accordion,
   AccordionDetails,
   AccordionSummary,
+  Badge,
+  Paper,
   Typography,
 } from "@mui/material";
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import { useState } from "react";
 import { Reservations } from "@/types";
+import {
+  DateCalendar,
+  DatePicker,
+  DatePickerToolbar,
+  LocalizationProvider,
+  PickersDay,
+  StaticDatePicker,
+  csCZ,
+} from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import CzechLocale from "dayjs/locale/cs";
+import dayjs from "dayjs";
+import * as isBetween from "dayjs/plugin/isBetween";
+dayjs.extend(isBetween as any);
+
+function makeDay(props: any) {
+  const { outsideCurrentMonth, reservations, day, ...other } = props;
+
+  const isValid = reservations.some(
+    ({ from_date, to_date }: { from_date: string; to_date: string }) =>
+      dayjs(day).isBetween(from_date, to_date, null, "[]")
+  );
+
+  return (
+    <PickersDay
+      {...other}
+      sx={{ backgroundColor: isValid && "error.main" }}
+      outsideCurrentMonth={outsideCurrentMonth}
+      day={day}
+    />
+  );
+}
 
 export default function ReservationDatesRender({
   reservations,
 }: {
-  reservations: Reservations;
+  reservations: Reservations[];
 }) {
-  const [expanded, setExpanded] = useState(false);
+  const [expanded, setExpanded] = useState(true);
   const isValid = true;
+  const [highlightedDays, setHighlightedDays] = useState([1, 2, 15]);
 
   return (
-    <Accordion expanded={expanded} onClick={() => setExpanded((c) => !c)}>
+    <Accordion expanded={expanded}>
       <AccordionSummary
+        onClick={() => setExpanded((c) => !c)}
         expandIcon={
           isValid && !expanded ? (
             <CheckCircleIcon color="success" />
@@ -35,10 +71,30 @@ export default function ReservationDatesRender({
         </div>
       </AccordionSummary>
       <AccordionDetails>
-        <Typography>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-          malesuada lacus ex, sit amet blandit leo lobortis eget.
-        </Typography>
+        <LocalizationProvider
+          dateAdapter={AdapterDayjs}
+          adapterLocale={CzechLocale as any}
+          localeText={
+            csCZ.components.MuiLocalizationProvider.defaultProps.localeText
+          }
+        >
+          <div className="flex gap-2">
+            <DateCalendar
+              sx={{ margin: 0 }}
+              slots={{
+                day: makeDay,
+              }}
+              slotProps={{
+                day: {
+                  reservations: reservations.map(({ from_date, to_date }) => ({
+                    from_date,
+                    to_date,
+                  })),
+                } as any,
+              }}
+            />
+          </div>
+        </LocalizationProvider>
       </AccordionDetails>
     </Accordion>
   );

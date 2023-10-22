a4454ffd074422dff0a99c8436d32c00644f20ef
OndrejHj04 <ondra.hajku@seznam.cz>
2023-10-22 20:11:24 +0200

    static date picker approach

--- app/reservations/create/components/ReservationDates/ReservationDatesRender.tsx
+++ app/reservations/create/components/ReservationDates/ReservationDatesRender.tsx
@@ -32,46 +32,6 @@ import CancelIcon from "@mui/icons-material/Cancel";
 import { useForm } from "react-hook-form";
 dayjs.extend(isBetween as any);
 
-function makeDay(props: any) {
-  const {
-    outsideCurrentMonth,
-    setSelectedDates,
-    selectedDates,
-    reservations,
-    day,
-    ...other
-  } = props;
-
-  const isReservation = reservations.some(
-    ({ from_date, to_date }: { from_date: string; to_date: string }) =>
-      dayjs(day).isBetween(from_date, to_date, null, "[]")
-  );
-
-  const isSelected =
-    dayjs(day).isSame(selectedDates[0], "day") ||
-    dayjs(day).isSame(selectedDates[1], "day");
-
-  const selectDate = () => {
-    if (!isReservation) {
-      if (selectedDates[0]) {
-        setSelectedDates([selectedDates[0], day]);
-      } else {
-        setSelectedDates([day, selectedDates[1]]);
-      }
-    }
-  };
-
-  return (
-    <PickersDay
-      {...other}
-      onClick={selectDate}
-      sx={{ backgroundColor: isSelected && "success.main" }}
-      outsideCurrentMonth={outsideCurrentMonth}
-      day={day}
-    />
-  );
-}
-
 export default function ReservationDatesRender({
   reservations,
 }: {
@@ -112,75 +72,31 @@ export default function ReservationDatesRender({
               csCZ.components.MuiLocalizationProvider.defaultProps.localeText
             }
           >
-            <div className="flex">
-              <DateCalendar
-                value={null}
-                sx={{ margin: 0 }}
-                slots={{
-                  day: makeDay,
-                }}
-                slotProps={{
-                  day: {
-                    reservations: reservations.map(
-                      ({ from_date, to_date }) => ({
-                        from_date,
-                        to_date,
-                      })
-                    ),
-                    selectedDates,
-                    setSelectedDates,
-                  } as any,
-                }}
+            <div className="flex gap-2">
+              <StaticDatePicker
+                sx={{ width: 200 }}
+                value={selectedDates[0]}
+                shouldDisableDate={(date) =>
+                  reservations.some((r) =>
+                    dayjs(date).isBetween(r.from_date, r.to_date, "day", "[]")
+                  )
+                }
+                onChange={(date) => setSelectedDates([date, selectedDates[1]])}
+                disableHighlightToday
+                slotProps={{ actionBar: { actions: ["cancel"] } }}
               />
-              <div className="flex flex-col gap-1">
-                <TextField
-                  label="Začátek rezervace"
-                  value={
-                    selectedDates[0]
-                      ? dayjs(selectedDates[0]).format("DD. MM. YYYY")
-                      : ""
-                  }
-                  InputProps={{
-                    endAdornment: (
-                      <InputAdornment position="end">
-                        <IconButton
-                          color="error"
-                          disabled={!selectedDates[0]}
-                          onClick={() =>
-                            setSelectedDates((day) => [null, day[1]])
-                          }
-                        >
-                          <CancelIcon />
-                        </IconButton>
-                      </InputAdornment>
-                    ),
-                  }}
-                />
-                <TextField
-                  label="Konec rezervace"
-                  value={
-                    selectedDates[1]
-                      ? dayjs(selectedDates[1]).format("DD. MM. YYYY")
-                      : ""
-                  }
-                  InputProps={{
-                    endAdornment: (
-                      <InputAdornment position="end">
-                        <IconButton
-                          color="error"
-                          disabled={!selectedDates[1]}
-                          onClick={() =>
-                            setSelectedDates((day) => [day[0], null])
-                          }
-                        >
-                          <CancelIcon />
-                        </IconButton>
-                      </InputAdornment>
-                    ),
-                  }}
-                />
-                <Button>Uložit</Button>
-              </div>
+              <StaticDatePicker
+                sx={{ width: 200 }}
+                value={selectedDates[1]}
+                shouldDisableDate={(date) =>
+                  reservations.some((r) =>
+                    dayjs(date).isBetween(r.from_date, r.to_date, "day", "[]")
+                  )
+                }
+                onChange={(date) => setSelectedDates([selectedDates[0], date])}
+                disableHighlightToday
+                slotProps={{ actionBar: { actions: ["cancel"] } }}
+              />
             </div>
           </LocalizationProvider>
         </AccordionDetails>

9d28c4ca5ea698ba12e3872cdbc239cbbdeada94
OndrejHj04 <ondra.hajku@seznam.cz>
2023-10-23 17:28:49 +0200

    members & groups checking 🗳️

--- app/reservations/create/components/ReservationMembers/ReservationMembers.tsx
+++ app/reservations/create/components/ReservationMembers/ReservationMembers.tsx
@@ -1,13 +1,17 @@
 import ReservationMembersRender from "./ReservationMembersRender";
 
 const getGroups = async () => {
-  const req = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/group/list`);
+  const req = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/group/list`, {
+    cache: "no-cache",
+  });
   const { data } = await req.json();
   return data;
 };
 
 const getUsers = async () => {
-  const req = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/users/list`);
+  const req = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/users/list`, {
+    cache: "no-cache",
+  });
   const { data } = await req.json();
   return data;
 };
--- app/reservations/create/components/ReservationMembers/ReservationMembersRender.tsx
+++ app/reservations/create/components/ReservationMembers/ReservationMembersRender.tsx
@@ -16,8 +16,8 @@ import {
 } from "@mui/material";
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import { useState } from "react";
-import { Group } from "@/types";
+import { useEffect, useState } from "react";
+import { Group, GroupOwner } from "@/types";
 import { User } from "next-auth";
 import AvatarWrapper from "@/ui-components/AvatarWrapper";
 
@@ -30,8 +30,14 @@ export default function ReservationMembersRender({
 }) {
   const [expanded, setExpanded] = useState(true);
   const isValid = true;
-  const [groupMembers, setGroupsMembers] = useState([]);
-  const [members, setMembers] = useState([]);
+  const [groupsIncluded, setGroupsIncluded] = useState<number[]>([]);
+  const [members, setMembers] = useState<number[]>([]);
+
+  const makeReset = () => {
+    setMembers([]);
+    setGroupsIncluded([]);
+  };
+
   return (
     <Accordion expanded={expanded}>
       <AccordionSummary
@@ -51,36 +57,67 @@ export default function ReservationMembersRender({
       </AccordionSummary>
       <AccordionDetails className="flex">
         <List subheader={<Typography variant="h6">Skupiny</Typography>}>
-          {groups.map((group) => (
-            <ListItemButton key={group.id}>
-              <Checkbox />
-              <ListItemIcon>
-                <Avatar>{group.name[0]}</Avatar>
-              </ListItemIcon>
-              <ListItemText
-                primary={group.name}
-                secondary={group.owner.email}
-              />
-            </ListItemButton>
-          ))}
+          {groups.map((group) => {
+            const isChecked = groupsIncluded.includes(group.id);
+            const handleClick = () => {
+              if (isChecked) {
+                setGroupsIncluded((c) => c.filter((i) => i !== group.id));
+                setMembers((c) =>
+                  c.filter((i) => !group.users.includes(i as any))
+                );
+              } else {
+                setGroupsIncluded((c) => [...c, group.id]);
+                setMembers((c) => [
+                  ...(new Set(c.concat(group.users as any)) as any),
+                ]);
+              }
+            };
+            return (
+              <ListItemButton key={group.id} onClick={handleClick}>
+                <Checkbox checked={isChecked} />
+                <ListItemIcon>
+                  <Avatar>{group.name[0].toUpperCase()}</Avatar>
+                </ListItemIcon>
+                <ListItemText
+                  primary={group.name}
+                  secondary={group.owner.email}
+                />
+              </ListItemButton>
+            );
+          })}
         </List>
         <List subheader={<Typography variant="h6">Uživatelé</Typography>}>
-          {users.map((user) => (
-            <ListItemButton key={user.id}>
-              <Checkbox />
-              <ListItemIcon>
-                <AvatarWrapper data={user} />
-              </ListItemIcon>
-              <ListItemText
-                primary={`${user.first_name} ${user.last_name}`}
-                secondary={user.email}
-              />
-            </ListItemButton>
-          ))}
+          {users.map((user) => {
+            const isChecked = members.includes(user.id);
+            const handleClick = () => {
+              if (isChecked) {
+                setMembers((c) => c.filter((i) => i !== user.id));
+              } else {
+                setMembers((c) => [...c, user.id]);
+              }
+            };
+            return (
+              <ListItemButton key={user.id} onClick={handleClick}>
+                <Checkbox checked={isChecked} />
+                <ListItemIcon>
+                  <AvatarWrapper data={user} />
+                </ListItemIcon>
+                <ListItemText
+                  primary={`${user.first_name} ${user.last_name}`}
+                  secondary={user.email}
+                />
+              </ListItemButton>
+            );
+          })}
         </List>
         <div className="flex flex-col gap-2">
           <Button variant="outlined">Uložit</Button>
-          <Button variant="contained" color="error">
+          <Button
+            variant="contained"
+            color="error"
+            disabled={!groupsIncluded.length && !members.length}
+            onClick={makeReset}
+          >
             Smazat
           </Button>
         </div>

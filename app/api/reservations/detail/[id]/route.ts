8e63167d373bfe381842da55dd1685923dbcaba6
OndrejHj04 <ondra.hajku@seznam.cz>
2023-10-26 14:04:39 +0200

    reservation routin´ ⛓️📨

--- app/api/reservations/detail/[id]/route.ts
+++ app/api/reservations/detail/[id]/route.ts
@@ -2,34 +2,44 @@ import { query } from "@/lib/db";
 import { Reservation } from "@/types";
 import { NextResponse } from "next/server";
 
-export async function GET(req: Request) {
+export async function GET(
+  req: Request,
+  { params: { id } }: { params: { id: string } }
+) {
   try {
     const data = (await query({
-      query: `SELECT * FROM reservations`,
-      values: [],
+      query: `SELECT * FROM reservations WHERE id = ?`,
+      values: [id],
     })) as any;
 
-    data.map((reservation: any) => {
+    data.forEach((reservation: any) => {
       reservation.groups = reservation.groups
         ? JSON.parse(reservation.groups as any)
         : [];
       reservation.users = reservation.users
         ? JSON.parse(reservation.users as any)
         : [];
-      return reservation;
     });
 
     const leader = (await query({
-      query: `SELECT id, email, first_name, last_name, image FROM users WHERE id IN(${data
-        .map((reservation: any) => reservation.leader)
-        .join(",")})`,
+      query: `SELECT id, email, first_name, last_name FROM users WHERE id IN(${data.map(
+        (reservation: any) => reservation.leader
+      )})`,
       values: [],
     })) as any;
 
     const groups = (await query({
-      query: `SELECT id, name FROM groups WHERE id IN(${[
-        ...(new Set(data.map((item: any) => item.groups).flat()) as any),
-      ]})`,
+      query: `SELECT * FROM groups WHERE id IN(${data.map((reservation: any) =>
+        reservation.groups.join(",")
+      )})`,
+      values: [],
+    })) as any;
+    groups.map((group: any) => (group.users = JSON.parse(group.users as any)));
+
+    const users = (await query({
+      query: `SELECT id, first_name, last_name, email, image FROM users WHERE id IN(${data.map(
+        (reservation: any) => reservation.users.join(",")
+      )})`,
       values: [],
     })) as any;
 
@@ -39,6 +49,9 @@ export async function GET(req: Request) {
       );
       reservation.groups = reservation.groups.map((group) =>
         groups.find((grp: any) => grp.id === group)
+      );
+      reservation.users = reservation.users.map((user) =>
+        users.find((grp: any) => grp.id === user)
       );
     });
 

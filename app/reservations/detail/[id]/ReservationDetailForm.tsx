6c939fd1065ee38fdf658016d4ea6200a790a743
OndrejHj04 <ondra.hajku@seznam.cz>
2023-10-27 17:02:41 +0200

    display groups && users

--- app/reservations/detail/[id]/ReservationDetailForm.tsx
+++ app/reservations/detail/[id]/ReservationDetailForm.tsx
@@ -3,10 +3,18 @@ import { Reservation } from "@/types";
 import AvatarWrapper from "@/ui-components/AvatarWrapper";
 import {
   Autocomplete,
+  Avatar,
   Box,
   Button,
   Card,
   CardHeader,
+  Checkbox,
+  Divider,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
   MenuItem,
   Paper,
   Select,
@@ -19,6 +27,8 @@ import { Controller, useForm } from "react-hook-form";
 import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import dayjs from "dayjs";
+import { useRouter } from "next/navigation";
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
 
 export default function ReservationDetailForm({
   reservation,
@@ -36,7 +46,9 @@ export default function ReservationDetailForm({
   const onSubmit = (data: any) => {
     console.log(data);
   };
-  console.log(reservation);
+
+  const { push } = useRouter();
+  const handleCheck = (user: any) => {};
   return (
     <>
       <form className="flex flex-col" onSubmit={handleSubmit(onSubmit)}>
@@ -48,58 +60,155 @@ export default function ReservationDetailForm({
             Uložit
           </Button>
         </div>
-        <Paper className="p-4 flex">
-          <Typography variant="h5">Vedoucí rezervace</Typography>
-          <AvatarWrapper data={reservation.leader as any} />
-          <Typography variant="body1" className="font-semibold">
-            {reservation.leader.first_name} {reservation.leader.last_name}
-          </Typography>
-          <Typography>{reservation.leader.email}</Typography>
-
-          <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <div className="flex flex-col gap-3 mx-3">
-              <Controller
-                control={control}
-                name="from_date"
-                defaultValue={dayjs(reservation.from_date)}
-                render={({ field: { onChange, value } }) => (
-                  <DatePicker
-                    value={value}
-                    onChange={onChange}
-                    label="Začátek rezervace"
-                    format="DD.MM.YYYY"
-                  />
-                )}
-              />
-              <Controller
-                control={control}
-                name="to_date"
-                defaultValue={dayjs(reservation.to_date)}
-                render={({ field: { onChange, value } }) => (
-                  <DatePicker
-                    value={value}
-                    onChange={onChange}
-                    label="Konec rezervace"
-                    format="DD.MM.YYYY"
-                  />
-                )}
-              />
+        <Paper className="p-4 flex flex-col gap-4">
+          <div className="flex">
+            <div className="flex flex-col gap-1">
+              <Typography variant="h5">Vedoucí rezervace</Typography>
+              <div className="flex gap-2">
+                <AvatarWrapper size={56} data={reservation.leader as any} />
+                <div className="flex flex-col">
+                  <Typography variant="h6" className="font-semibold">
+                    {reservation.leader.first_name}{" "}
+                    {reservation.leader.last_name}
+                  </Typography>
+                  <Typography>{reservation.leader.email}</Typography>
+                </div>
+              </div>
             </div>
-          </LocalizationProvider>
-          <div className="flex flex-col gap-3">
-            <TextField
-              {...register("purpouse")}
-              label="Účel rezervace"
-              defaultValue={reservation.purpouse}
-            />
-            <Select defaultValue={reservation.rooms} {...register("rooms")}>
-              {[...Array(5)].map((_, i) => (
-                <MenuItem key={i} value={i + 1}>
-                  {i + 1} Pokojů
-                </MenuItem>
-              ))}
-              <MenuItem value={6}>Celá chata</MenuItem>
-            </Select>
+
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <div className="flex flex-col gap-3 mx-3">
+                <Controller
+                  control={control}
+                  name="from_date"
+                  defaultValue={dayjs(reservation.from_date)}
+                  render={({ field: { onChange, value } }) => (
+                    <DatePicker
+                      value={value}
+                      onChange={onChange}
+                      label="Začátek rezervace"
+                      format="DD.MM.YYYY"
+                    />
+                  )}
+                />
+                <Controller
+                  control={control}
+                  name="to_date"
+                  defaultValue={dayjs(reservation.to_date)}
+                  render={({ field: { onChange, value } }) => (
+                    <DatePicker
+                      value={value}
+                      onChange={onChange}
+                      label="Konec rezervace"
+                      format="DD.MM.YYYY"
+                    />
+                  )}
+                />
+              </div>
+            </LocalizationProvider>
+            <div className="flex flex-col gap-3">
+              <TextField
+                {...register("purpouse")}
+                label="Účel rezervace"
+                defaultValue={reservation.purpouse}
+              />
+              <Select defaultValue={reservation.rooms} {...register("rooms")}>
+                {[...Array(5)].map((_, i) => (
+                  <MenuItem key={i} value={i + 1}>
+                    {i + 1} Pokojů
+                  </MenuItem>
+                ))}
+                <MenuItem value={6}>Celá chata</MenuItem>
+              </Select>
+            </div>
+          </div>
+          <div className="flex gap-2">
+            <div className="flex flex-col">
+              <Typography variant="h5">Uživatelé v rezervaci</Typography>
+              <Divider />
+              <List sx={{ height: 400, overflowY: "auto" }}>
+                {reservation.users.length ? (
+                  reservation.users.map((user: any) => (
+                    <ListItem disablePadding key={user.id}>
+                      <ListItemButton
+                        sx={{ padding: 1 }}
+                        onClick={() => push(`/user/detail/${user.id}`)}
+                      >
+                        <ListItemIcon>
+                          <AvatarWrapper data={user} />
+                        </ListItemIcon>
+                        <ListItemText
+                          primary={
+                            <Typography>
+                              {user.first_name} {user.last_name}
+                            </Typography>
+                          }
+                          secondary={user.email}
+                        />
+                        <Checkbox disableRipple />
+                      </ListItemButton>
+                    </ListItem>
+                  ))
+                ) : (
+                  <>
+                    <Typography>Žádní uživatelé ve rezervaci</Typography>
+                  </>
+                )}
+              </List>
+              <div className="flex flex-col gap-2">
+                <Button
+                  variant="contained"
+                  color="error"
+                  endIcon={<DeleteForeverIcon />}
+                >
+                  Odebrat vybrané uživatele
+                </Button>
+                <Button variant="contained" endIcon={<DeleteForeverIcon />}>
+                  Přidat skupiny
+                </Button>
+              </div>
+            </div>
+            <div className="flex flex-col">
+              <Typography variant="h5">Skupiny v rezervaci</Typography>
+              <Divider />
+              <List sx={{ height: 400, overflowY: "auto" }}>
+                {reservation.groups.length ? (
+                  reservation.groups.map((group: any) => (
+                    <ListItem disablePadding key={group.id}>
+                      <ListItemButton
+                        sx={{ padding: 1 }}
+                        onClick={() => push(`/group/detail/${group.id}`)}
+                      >
+                        <ListItemIcon>
+                          <Avatar />
+                        </ListItemIcon>
+                        <ListItemText
+                          primary={group.name}
+                          secondary={"Počet členů: " + group.users.length}
+                        />
+                        <Checkbox disableRipple />
+                      </ListItemButton>
+                    </ListItem>
+                  ))
+                ) : (
+                  <>
+                    <Typography>Žádné skuiny v rezervaci</Typography>
+                  </>
+                )}
+              </List>
+              <div className="flex flex-col gap-2 ">
+                <Button
+                  variant="contained"
+                  color="error"
+                  endIcon={<DeleteForeverIcon />}
+                >
+                  Odebrat vybrané skupiny
+                </Button>
+                <Button variant="contained" endIcon={<DeleteForeverIcon />}>
+                  Přidat skupiny
+                </Button>
+              </div>
+            </div>
           </div>
         </Paper>
       </form>

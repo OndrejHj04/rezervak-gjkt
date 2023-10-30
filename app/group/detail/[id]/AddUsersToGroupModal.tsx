baaa7aeb688ebd91ea4b564d99b6378ba84f79d8
OndrejHj04 <ondra.hajku@seznam.cz>
2023-10-30 16:30:43 +0100

    add to group

---
+++ app/group/detail/[id]/AddUsersToGroupModal.tsx
@@ -0,0 +1,118 @@
+"use client";
+
+import AvatarWrapper from "@/ui-components/AvatarWrapper";
+import {
+  Autocomplete,
+  Box,
+  Button,
+  CircularProgress,
+  Paper,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
+import { Controller, useForm } from "react-hook-form";
+import { toast } from "react-toastify";
+import MakeGroupDetailRefetch from "./refetch";
+
+const style = {
+  position: "absolute" as "absolute",
+  top: "50%",
+  left: "50%",
+  outline: "none",
+  transform: "translate(-50%, -50%)",
+};
+
+export default function AddUsersToGroupModal({
+  setModal,
+  currentUsers,
+  groupId,
+}: {
+  setModal: Dispatch<SetStateAction<boolean>>;
+  currentUsers: number[];
+  groupId: number;
+}) {
+  const [users, setUsers] = useState(null);
+  const {
+    handleSubmit,
+    control,
+    register,
+    formState: { isValid },
+  } = useForm();
+
+  useEffect(() => {
+    fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/users/list`)
+      .then((res) => res.json())
+      .then((res) => setUsers(res.data));
+  }, []);
+
+  const onSubmit = (data: any) => {
+    fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/group/add-member`, {
+      method: "POST",
+      body: JSON.stringify({
+        group: groupId,
+        newMembers: data.users.map((group: any) => group.id),
+        currentMembers: currentUsers,
+      }),
+    })
+      .then((req) => req.json())
+      .then((data) => toast.success("Skupiny úspěšně přidány"))
+      .catch((err) => toast.error("Něco se nepovedlo"))
+      .finally(() => {
+        MakeGroupDetailRefetch(groupId);
+        setModal(false);
+      });
+  };
+
+  return (
+    <Paper sx={style} className="p-2 ">
+      <Typography variant="h5" className="mb-2 text-center">
+        Přidat uživatele
+      </Typography>
+      <form className="flex flex-col gap-2" onSubmit={handleSubmit(onSubmit)}>
+        {users ? (
+          <Controller
+            control={control}
+            {...register("users", { required: true })}
+            render={({ field: { value, onChange } }) => (
+              <Autocomplete
+                value={value}
+                onChange={(e, value) => {
+                  onChange(value);
+                }}
+                sx={{ width: 300 }}
+                multiple
+                filterSelectedOptions
+                getOptionLabel={(option: any) =>
+                  `${option.first_name} ${option.last_name}`
+                }
+                getOptionDisabled={(option: any) =>
+                  currentUsers.includes(option.id)
+                }
+                options={users}
+                renderOption={(props: any, option: any) => (
+                  <div {...props}>
+                    <Box className="flex items-center gap-2">
+                      <AvatarWrapper data={option} />
+                      <Typography className="ml-2">
+                        {option.first_name} {option.last_name}
+                      </Typography>
+                    </Box>
+                  </div>
+                )}
+                renderInput={(params) => (
+                  <TextField {...params} label="Vybrat uživatele..." />
+                )}
+              />
+            )}
+          />
+        ) : (
+          <CircularProgress />
+        )}
+        <Button variant="contained" type="submit" disabled={!isValid}>
+          Uložit
+        </Button>
+      </form>
+    </Paper>
+  );
+}
--- app/group/detail/[id]/GroupDetailForm.tsx
+++ app/group/detail/[id]/GroupDetailForm.tsx
@@ -17,6 +17,7 @@ import {
   ListItemButton,
   ListItemIcon,
   ListItemText,
+  Modal,
   Paper,
   TextField,
   Typography,
@@ -32,6 +33,7 @@ import dayjs from "dayjs";
 import MakeRefetch from "../../list/refetch";
 import { useRouter } from "next/navigation";
 import MakeGroupDetailRefetch from "./refetch";
+import AddUsersToGroupModal from "./AddUsersToGroupModal";
 
 interface selecteUser {
   label: string;
@@ -44,6 +46,7 @@ interface selecteUser {
 export default function GroupDetailForm({ group }: { group: Group }) {
   const { push } = useRouter();
   const [checked, setChecked] = useState<number[]>([]);
+  const [usersModal, setUsersModal] = useState(false);
   const {
     formState: { isDirty },
     register,
@@ -99,6 +102,7 @@ export default function GroupDetailForm({ group }: { group: Group }) {
       .catch((e) => toast.error("Něco se nepovedlo"))
       .finally(() => {
         MakeGroupDetailRefetch(group.id);
+        setChecked([]);
       });
   };
 
@@ -119,174 +123,185 @@ export default function GroupDetailForm({ group }: { group: Group }) {
   }
 
   return (
-    <form className="flex flex-col" onSubmit={handleSubmit(onSubmit)}>
-      <div className="mb-2 ml-auto flex gap-2">
-        <Button variant="outlined" color="error" onClick={handleRemoveGroup}>
-          Odstranit
-        </Button>
-        <Button variant="outlined" type="submit" disabled={!isDirty}>
-          Uložit
-        </Button>
-      </div>
-      <Paper className="flex gap-4 p-4 flex-col">
-        <div className="flex gap-2">
-          <div className="flex flex-col gap-1">
-            <Typography variant="h5">Vedoucí skupiny</Typography>
-            <div className="flex gap-2">
-              <AvatarWrapper size={56} data={group.owner as any} />
-              <div className="flex flex-col">
-                <Typography variant="h6" className="font-semibold">
-                  {group.owner.first_name} {group.owner.last_name}
-                </Typography>
-                <Typography>{group.owner.email}</Typography>
+    <>
+      {usersModal && (
+        <Modal open={usersModal} onClose={() => setUsersModal(false)}>
+          <AddUsersToGroupModal
+            groupId={group.id}
+            setModal={setUsersModal}
+            currentUsers={group.users.map((user: any) => user.id)}
+          />
+        </Modal>
+      )}
+      <form className="flex flex-col" onSubmit={handleSubmit(onSubmit)}>
+        <div className="mb-2 ml-auto flex gap-2">
+          <Button variant="outlined" color="error" onClick={handleRemoveGroup}>
+            Odstranit
+          </Button>
+          <Button variant="outlined" type="submit" disabled={!isDirty}>
+            Uložit
+          </Button>
+        </div>
+        <Paper className="flex gap-4 p-4 flex-col">
+          <div className="flex gap-2">
+            <div className="flex flex-col gap-1">
+              <Typography variant="h5">Vedoucí skupiny</Typography>
+              <div className="flex gap-2">
+                <AvatarWrapper size={56} data={group.owner as any} />
+                <div className="flex flex-col">
+                  <Typography variant="h6" className="font-semibold">
+                    {group.owner.first_name} {group.owner.last_name}
+                  </Typography>
+                  <Typography>{group.owner.email}</Typography>
+                </div>
+              </div>
+              <div className="flex ">
+                <Typography variant="h6">Počet členů:</Typography>
+                <Chip
+                  label={`${group.users.length} členů`}
+                  sx={{
+                    margin: "auto",
+                  }}
+                  icon={<Icon>group_icon</Icon>}
+                />
               </div>
             </div>
-            <div className="flex ">
-              <Typography variant="h6">Počet členů:</Typography>
-              <Chip
-                label={`${group.users.length} členů`}
-                sx={{
-                  margin: "auto",
-                }}
-                icon={<Icon>group_icon</Icon>}
+            <div className="flex gap-2">
+              <TextField
+                label="Jméno"
+                {...register("name")}
+                defaultValue={group.name}
+              />
+              <TextField
+                {...register("description")}
+                defaultValue={group.description}
+                multiline
+                label="Popis skupiny"
+                minRows={4}
+                maxRows={4}
               />
             </div>
           </div>
-          <div className="flex gap-2">
-            <TextField
-              label="Jméno"
-              {...register("name")}
-              defaultValue={group.name}
-            />
-            <TextField
-              {...register("description")}
-              defaultValue={group.description}
-              multiline
-              label="Popis skupiny"
-              minRows={4}
-              maxRows={4}
-            />
-          </div>
-        </div>
 
-        <div className="flex gap-2">
-          <div className="flex flex-col">
-            <Typography variant="h5">
-              Uživatelé ve skupině{" "}
-              {group.users.length && <span>({group.users.length})</span>}
-            </Typography>
-            <Divider />
-            <List sx={{ height: 400 }}>
-              {group.users.length ? (
-                group.users.map((user: any) => (
-                  <ListItem disablePadding key={user.id}>
-                    <ListItemButton
-                      sx={{ padding: 1 }}
-                      onClick={() => handleCheck(user.id)}
-                    >
-                      <ListItemIcon>
-                        <AvatarWrapper data={user} />
-                      </ListItemIcon>
-                      <ListItemText
-                        primary={
-                          <Typography>
-                            {user.first_name} {user.last_name}
-                          </Typography>
-                        }
-                        secondary={user.email}
-                      />
-                      <Checkbox
-                        disableRipple
-                        checked={checked.includes(user.id)}
-                      />
-                      <IconButton
-                        onClick={(e) => push(`/user/detail/${user.id}`)}
+          <div className="flex gap-2">
+            <div className="flex flex-col">
+              <Typography variant="h5">
+                Uživatelé ve skupině{" "}
+                {group.users.length && <span>({group.users.length})</span>}
+              </Typography>
+              <Divider />
+              <List sx={{ height: 400 }}>
+                {group.users.length ? (
+                  group.users.map((user: any) => (
+                    <ListItem disablePadding key={user.id}>
+                      <ListItemButton
+                        sx={{ padding: 1 }}
+                        onClick={() => handleCheck(user.id)}
                       >
-                        <Icon>info_icon</Icon>
-                      </IconButton>
-                    </ListItemButton>
-                  </ListItem>
-                ))
-              ) : (
-                <>
-                  <Typography>Žádní uživatelé ve skupině</Typography>
-                </>
-              )}
-            </List>
-            <div className="flex flex-col gap-2">
-              <Button
-                variant="contained"
-                color="error"
-                endIcon={<DeleteForeverIcon />}
-                disabled={checked.length === 0}
-                onClick={handleDeleteMembers}
-              >
-                Odebrat vybrané uživatele ze skupiny
-              </Button>
-              <Button
-                variant="contained"
-                onClick={() => {}}
-                endIcon={<AddToPhotosIcon />}
-              >
-                Přidat uživatele do skupiny
-              </Button>
+                        <ListItemIcon>
+                          <AvatarWrapper data={user} />
+                        </ListItemIcon>
+                        <ListItemText
+                          primary={
+                            <Typography>
+                              {user.first_name} {user.last_name}
+                            </Typography>
+                          }
+                          secondary={user.email}
+                        />
+                        <Checkbox
+                          disableRipple
+                          checked={checked.includes(user.id)}
+                        />
+                        <IconButton
+                          onClick={(e) => push(`/user/detail/${user.id}`)}
+                        >
+                          <Icon>info_icon</Icon>
+                        </IconButton>
+                      </ListItemButton>
+                    </ListItem>
+                  ))
+                ) : (
+                  <>
+                    <Typography>Žádní uživatelé ve skupině</Typography>
+                  </>
+                )}
+              </List>
+              <div className="flex flex-col gap-2">
+                <Button
+                  variant="contained"
+                  color="error"
+                  endIcon={<DeleteForeverIcon />}
+                  disabled={checked.length === 0}
+                  onClick={handleDeleteMembers}
+                >
+                  Odebrat vybrané uživatele ze skupiny
+                </Button>
+                <Button
+                  variant="contained"
+                  onClick={() => setUsersModal(true)}
+                  endIcon={<AddToPhotosIcon />}
+                >
+                  Přidat uživatele do skupiny
+                </Button>
+              </div>
+            </div>
+            <div className="flex flex-col">
+              <Typography variant="h5">
+                Rezervace skupiny{" "}
+                {group.reservations.length && (
+                  <span>({group.reservations.length})</span>
+                )}
+              </Typography>
+              <Divider />
+              <List sx={{ height: 400 }}>
+                {group.reservations.length ? (
+                  group.reservations.map((reservation: any) => (
+                    <ListItem disablePadding key={reservation.id}>
+                      <ListItemButton sx={{ padding: 1 }} onClick={() => {}}>
+                        <ListItemIcon>
+                          <Avatar />
+                        </ListItemIcon>
+                        <ListItemText
+                          primary={<Typography>{reservation.name}</Typography>}
+                          secondary={`${dayjs(reservation.from_date).format(
+                            "DD.MM.YYYY"
+                          )} - ${dayjs(reservation.to_date).format(
+                            "DD.MM.YYYY"
+                          )}`}
+                        />
+                        <Checkbox disableRipple />
+                        <IconButton
+                          onClick={(e) =>
+                            push(`/reservations/detail/${reservation.id}`)
+                          }
+                        >
+                          <Icon>info_icon</Icon>
+                        </IconButton>
+                      </ListItemButton>
+                    </ListItem>
+                  ))
+                ) : (
+                  <>
+                    <Typography>Žádní rezervace skupiny</Typography>
+                  </>
+                )}
+              </List>
+              <div className="flex flex-col gap-2">
+                <Button
+                  variant="contained"
+                  color="error"
+                  endIcon={<DeleteForeverIcon />}
+                  disabled
+                  onClick={() => {}}
+                >
+                  Odebrat z vybraných rezervací
+                </Button>
+              </div>
             </div>
           </div>
-          <div className="flex flex-col">
-            <Typography variant="h5">
-              Rezervace skupiny{" "}
-              {group.reservations.length && (
-                <span>({group.reservations.length})</span>
-              )}
-            </Typography>
-            <Divider />
-            <List sx={{ height: 400 }}>
-              {group.reservations.length ? (
-                group.reservations.map((reservation: any) => (
-                  <ListItem disablePadding key={reservation.id}>
-                    <ListItemButton sx={{ padding: 1 }} onClick={() => {}}>
-                      <ListItemIcon>
-                        <Avatar />
-                      </ListItemIcon>
-                      <ListItemText
-                        primary={<Typography>{reservation.name}</Typography>}
-                        secondary={`${dayjs(reservation.from_date).format(
-                          "DD.MM.YYYY"
-                        )} - ${dayjs(reservation.to_date).format(
-                          "DD.MM.YYYY"
-                        )}`}
-                      />
-                      <Checkbox disableRipple />
-                      <IconButton
-                        onClick={(e) =>
-                          push(`/reservations/detail/${reservation.id}`)
-                        }
-                      >
-                        <Icon>info_icon</Icon>
-                      </IconButton>
-                    </ListItemButton>
-                  </ListItem>
-                ))
-              ) : (
-                <>
-                  <Typography>Žádní rezervace skupiny</Typography>
-                </>
-              )}
-            </List>
-            <div className="flex flex-col gap-2">
-              <Button
-                variant="contained"
-                color="error"
-                endIcon={<DeleteForeverIcon />}
-                disabled
-                onClick={() => {}}
-              >
-                Odebrat z vybraných rezervací
-              </Button>
-            </div>
-          </div>
-        </div>
-      </Paper>
-    </form>
+        </Paper>
+      </form>
+    </>
   );
 }

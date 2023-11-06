3b7d17d0a30a5ddb3071f17e4aa574c50dedcfd6
OndrejHj04 <ondra.hajku@seznam.cz>
2023-11-06 06:54:54 +0100

    test

--- app/api/reservations/delete/route.ts
+++ app/api/reservations/delete/route.ts
@@ -3,18 +3,21 @@ import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
   try {
+    console.log("WELCOME");
     const { reservations } = await req.json();
-
     const getReservations = (await query({
       query: `SELECT * FROM reservations WHERE id IN (${reservations.join(
         ","
       )})`,
       values: [],
     })) as any;
+    console.log("1");
+
     getReservations.forEach((reservation: any) => {
       reservation.groups = JSON.parse(reservation.groups);
       reservation.users = JSON.parse(reservation.users);
     });
+    console.log("2");
 
     const groupReservations = (await query({
       query: `SELECT id, reservations FROM ${"`groups`"} WHERE id IN (${getReservations
@@ -23,6 +26,7 @@ export async function POST(req: Request) {
         .join(",")})`,
       values: [],
     })) as any;
+    console.log("3");
 
     groupReservations.map((group: any) => {
       let reservations = JSON.parse(group.reservations);
@@ -36,6 +40,7 @@ export async function POST(req: Request) {
         values: [],
       });
     });
+    console.log("4");
 
     const usersReservations = (await query({
       query: `SELECT id, reservations FROM users WHERE id IN (${getReservations
@@ -45,6 +50,7 @@ export async function POST(req: Request) {
       values: [],
     })) as any;
 
+    console.log("5");
     usersReservations.map((user: any) => {
       let reservations = JSON.parse(user.reservations);
       reservations = reservations.filter(
@@ -57,11 +63,13 @@ export async function POST(req: Request) {
         values: [],
       });
     });
+    console.log("6");
 
     const data = await query({
       query: `DELETE FROM reservations WHERE id IN (${reservations.join(",")})`,
       values: [],
     });
+    console.log("7");
 
     return NextResponse.json({
       success: true,

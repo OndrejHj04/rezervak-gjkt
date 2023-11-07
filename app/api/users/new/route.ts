5fa3d964fc6bbd3c0459b842d1dd8efdca63942f
OndrejHj04 <ondra.hajku@seznam.cz>
2023-11-07 06:41:29 +0100

    fix

--- app/api/users/new/route.ts
+++ app/api/users/new/route.ts
@@ -2,13 +2,15 @@ import { query } from "@/lib/db";
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
-  const { first_name, last_name, email, role, parent } = await req.json();
+  const { first_name, last_name, email, role } = await req.json();
   const password = Math.random().toString(36).slice(-9);
-
+  console.log(
+    `INSERT INTO users(first_name, last_name, email, role, password) VALUES("${first_name}", "${last_name}", "${email}", ${role}, "${password}")`
+  );
   try {
     const data = await query({
-      query: `INSERT INTO users(first_name, last_name, email, role, password) VALUES(?, ?, ?, ?, ?)`,
-      values: [first_name, last_name, email, role, password, parent],
+      query: `INSERT INTO users(first_name, last_name, email, role, password) VALUES("${first_name}", "${last_name}", "${email}", ${role}, "${password}")`,
+      values: [first_name, last_name, email, role, password],
     });
 
     return NextResponse.json({

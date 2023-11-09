6776dd4f8c71711cc90539bd263850d26075134f
OndrejHj04 <ondra.hajku@seznam.cz>
2023-11-09 16:38:24 +0100

    register form

--- app/navigation/modals/ImportUsers.tsx
+++ app/navigation/modals/ImportUsers.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import { store } from "@/store/store";
-import { Modal, Paper, Typography } from "@mui/material";
+import { Button, Input, Modal, Paper, Typography } from "@mui/material";
+import { useForm } from "react-hook-form";
 
 const style = {
   position: "absolute" as "absolute",
@@ -14,18 +15,42 @@ const style = {
 export default function ImportUsers() {
   const { modal, setModal } = store();
   const close = () => setModal("");
+  const {
+    register,
+    formState: { isValid },
+    handleSubmit,
+    watch,
+  } = useForm();
+
+  const onSubmit = (data: any) => {
+    console.log(data.file);
+  };
 
   return (
     <Modal
-      open={modal === "importUsers"}
+      open={true}
       onClose={close}
       aria-labelledby="modal-modal-title"
       aria-describedby="modal-modal-description"
     >
-      <Paper sx={style} className="p-2 flex gap-2 flex-col">
-        <Typography variant="h5" className="text-center">
-          Importovat uživatele
-        </Typography>
+      <Paper sx={style} className="p-2 ">
+        <form className="flex gap-2 flex-col" onSubmit={handleSubmit(onSubmit)}>
+          <Typography variant="h5" className="text-center">
+            Importovat uživatele
+          </Typography>
+          <Button variant="contained" component="label">
+            {watch("file") ? watch("file")[0].name : <span>Vybrat soubor</span>}
+
+            <input
+              type="file"
+              hidden
+              {...register("file", { required: true })}
+            />
+          </Button>
+          <Button variant="contained" type="submit" disabled={!isValid}>
+            Nahrát
+          </Button>
+        </form>
       </Paper>
     </Modal>
   );

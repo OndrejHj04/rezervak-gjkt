a3002bd8ff721aad30d97377b95a7a650c492a60
OndrejHj04 <ondra.hajku@seznam.cz>
2023-11-10 09:36:50 +0100

    import csv users! 📎📑📜

--- app/navigation/modals/ImportUsers.tsx
+++ app/navigation/modals/ImportUsers.tsx
@@ -5,6 +5,8 @@ import { Button, Input, Modal, Paper, Typography } from "@mui/material";
 import { useForm } from "react-hook-form";
 import Papa from "papaparse";
 import { toast } from "react-toastify";
+import { useEffect, useState } from "react";
+import { parse } from "path";
 
 const style = {
   position: "absolute" as "absolute",
@@ -14,7 +16,10 @@ const style = {
   transform: "translate(-50%, -50%)",
 };
 
+const importUsersValidFormat = ["first_name", "last_name", "email", "role"];
+
 export default function ImportUsers() {
+  const [status, setStatus] = useState({ valid: false, message: "" });
   const { modal, setModal } = store();
   const close = () => setModal("");
   const {
@@ -24,6 +29,69 @@ export default function ImportUsers() {
     handleSubmit,
     watch,
   } = useForm();
+  const input = watch("file");
+
+  useEffect(() => {
+    if (input && input.length) {
+      validateFile(input).then((res) => {
+        if (res.titleRowFormat && res.validRows.length) {
+          setStatus({
+            valid: true,
+            message: `Data v souboru jsou ve správném formátu. Je zde ${res.validRows.length} platných řádků k nahrání.`,
+          });
+        } else {
+          setStatus({
+            valid: false,
+            message: `Data v souboru nejsou ve správném formátu. Soubor musí respektovat následující formát: ${importUsersValidFormat.join(
+              ","
+            )}.`,
+          });
+        }
+      });
+    } else {
+      setStatus({
+        valid: false,
+        message: ``,
+      });
+    }
+  }, [input]);
+
+  const makeParse = async (data: any) => {
+    return (await new Promise((resolve) => {
+      Papa.parse(data[0], {
+        encoding: "UTF-8",
+        complete: (results: any) => resolve(results.data),
+      });
+    })) as any;
+  };
+
+  const validateFile = async (data: any) => {
+    const format = await makeParse(data);
+
+    const titleRowFormat =
+      JSON.stringify(format[0]) === JSON.stringify(importUsersValidFormat);
+    const validRows = format
+      .slice(1)
+      .filter(
+        (row: any) => row.length === 4 && row.every((item: any) => item.length)
+      );
+
+    return { titleRowFormat, validRows };
+  };
+
+  const onSubmit = async (data: any) => {
+    const { validRows } = (await validateFile(data.file)) as any;
+
+    const newData = validRows.map((row: any) => {
+      let object = {} as any;
+      row.forEach((item: any, i: any) => {
+        object[importUsersValidFormat[i]] = item;
+      });
+      return object;
+    });
+
+    handleRequest(newData);
+  };
 
   const handleRequest = (newData: any) => {
     fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/users/import-new`, {
@@ -32,7 +100,6 @@ export default function ImportUsers() {
     })
       .then((res) => res.json())
       .then((res) => {
-        console.log(res);
         if (res.success) {
           toast.success(res.message);
           reset();
@@ -41,33 +108,12 @@ export default function ImportUsers() {
           toast.error(res.message);
         }
       })
-      .catch((e) => console.log(e));
-  };
-
-  const onSubmit = async (data: any) => {
-    const parse = (await new Promise((resolve) => {
-      Papa.parse(data.file[0], {
-        encoding: "UTF-8",
-        complete: (results: any) => resolve(results.data),
-      });
-    })) as any;
-    const planObject = parse[0] as any;
-    const filteredRows = parse.filter(
-      (row: any) => row.length === planObject.length && row !== planObject
-    );
-    const newData = filteredRows.map((row: any) => {
-      let object = {} as any;
-      row.forEach((item: any, i: any) => {
-        object[planObject[i]] = item;
-      });
-      return object;
-    });
-    handleRequest(newData);
+      .catch((e) => toast.error(e.message));
   };
 
   return (
     <Modal
-      open={modal === "importUsers"}
+      open={modal === "import"}
       onClose={close}
       aria-labelledby="modal-modal-title"
       aria-describedby="modal-modal-description"
@@ -78,7 +124,7 @@ export default function ImportUsers() {
             Importovat uživatele
           </Typography>
           <Button variant="contained" component="label">
-            {watch("file") ? watch("file")[0].name : <span>Vybrat soubor</span>}
+            {input && input.length ? input[0].name : "Vybrat soubror"}
 
             <input
               type="file"
@@ -87,7 +133,19 @@ export default function ImportUsers() {
               accept=".csv"
             />
           </Button>
-          <Button variant="contained" type="submit" disabled={!isValid}>
+          {!!status.message.length && (
+            <Typography
+              color={status.valid ? "success.main" : "error.main"}
+              className="font-semibold text-center"
+            >
+              {status.message}
+            </Typography>
+          )}
+          <Button
+            variant="contained"
+            type="submit"
+            disabled={!isValid || !status.valid}
+          >
             Nahrát
           </Button>
         </form>

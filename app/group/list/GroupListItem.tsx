9f9f2b06a3a7035cf7676b44cdf44d70d7af4296
OndrejHj04 <ondra.hajku@seznam.cz>
2023-11-12 14:41:09 +0100

    groups table item routing

--- app/group/list/GroupListItem.tsx
+++ app/group/list/GroupListItem.tsx
@@ -1,35 +1,21 @@
-"use client";
 import { Group } from "@/types";
 import AvatarWrapper from "@/ui-components/AvatarWrapper";
-import { Checkbox, IconButton, TableCell, TableRow } from "@mui/material";
-import Diversity3Icon from "@mui/icons-material/Diversity3";
-import { useRouter } from "next/navigation";
-import { Dispatch, SetStateAction } from "react";
-import { store } from "@/store/store";
+import {
+  Button,
+  Checkbox,
+  IconButton,
+  Table,
+  TableCell,
+  TableRow,
+} from "@mui/material";
+
+import GroupCheckbox from "./GroupCheckbox";
+import Link from "next/link";
 
 export default function GroupListItem({ group }: { group: Group }) {
-  const { selectedGroups, setSelectedGroups } = store();
-  const { push } = useRouter();
-  const checked = selectedGroups.includes(group.id);
-
-  const handleSelect = (e: React.MouseEvent<HTMLButtonElement>) => {
-    e.stopPropagation();
-    if (selectedGroups.includes(group.id)) {
-      setSelectedGroups(selectedGroups.filter((id) => id !== group.id));
-    } else {
-      setSelectedGroups([...selectedGroups, group.id]);
-    }
-  };
-
   return (
-    <TableRow
-      hover
-      className="cursor-pointer"
-      onClick={() => push(`/group/detail/${group.id}`)}
-    >
-      <TableCell>
-        <Checkbox onClick={handleSelect} checked={Boolean(checked)} />
-      </TableCell>
+    <TableRow className="cursor-pointer">
+      <GroupCheckbox group={group} />
       <TableCell>{group.name}</TableCell>
       <TableCell>{group.description}</TableCell>
       <TableCell>{group.users.length}</TableCell>
@@ -37,9 +23,9 @@ export default function GroupListItem({ group }: { group: Group }) {
         <AvatarWrapper data={group.owner} />
       </TableCell>
       <TableCell>
-        <IconButton>
-          <Diversity3Icon />
-        </IconButton>
+        <Link href={`/group/detail/${group.id}`}>
+          <Button>Detail</Button>
+        </Link>
       </TableCell>
     </TableRow>
   );

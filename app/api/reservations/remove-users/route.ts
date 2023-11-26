c3179201ec5bcddcef339afaa34b5a36fdc7e93c
OndrejHj04 <ondra.hajku@seznam.cz>
2023-11-26 16:30:04 +0100

    test

--- app/api/reservations/remove-users/route.ts
+++ app/api/reservations/remove-users/route.ts
@@ -2,26 +2,29 @@ import { query } from "@/lib/db";
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
+  console.log("start");
   try {
     const { reservation, removeUsers, currentUsers } = await req.json();
-
+    console.log(reservation, removeUsers, currentUsers);
     const getReservation = await query({
       query: `UPDATE reservations SET users = "${JSON.stringify(
         currentUsers.filter((user: any) => !removeUsers.includes(user))
       )}" WHERE id = ${reservation}`,
       values: [],
     });
-
+    console.log('1')
     const users = (await query({
       query: `SELECT id, reservations FROM users WHERE id IN (${removeUsers.join(
         ","
       )})`,
       values: [],
     })) as any;
+    console.log('2')
 
     users.forEach((user: any) => {
       user.reservations = JSON.parse(user.reservations);
     });
+    console.log('3')
 
     users.map(async (user: any) => {
       let newReservation = (user.reservations = user.reservations.filter(
@@ -42,9 +45,12 @@ export async function POST(req: Request) {
       data: users,
     });
   } catch (e) {
-    return NextResponse.json({
-      success: false,
-      message: "Something went wrong",
-    },{ status: 500 });
+    return NextResponse.json(
+      {
+        success: false,
+        message: "Something went wrong",
+      },
+      { status: 500 }
+    );
   }
 }

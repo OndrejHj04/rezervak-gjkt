0e4cbc22eaae5f7cd9942db9f0035bb3b252758a
OndrejHj04 <ondra.hajku@seznam.cz>
2023-12-31 16:13:40 +0100

    dark mode toggle

--- app/navigation/topbar/DarkModeToggle.tsx
+++ app/navigation/topbar/DarkModeToggle.tsx
@@ -3,12 +3,15 @@ import { setTheme } from "@/app/admin/actions/actionts";
 import DarkModeIcon from "@mui/icons-material/DarkMode";
 import LightModeIcon from "@mui/icons-material/LightMode";
 import { IconButton } from "@mui/material";
+import { useSession } from "next-auth/react";
 
 const handleSetTheme = ({ theme, id }: { theme: any; id: any }) => {
   setTheme(theme, id).then(() => window.location.reload());
 };
 
 export default function DarkModeToggle({ theme, id }: { theme: any; id: any }) {
+  const { status } = useSession();
+  if (status === "unauthenticated") return null;
   return (
     <IconButton onClick={() => handleSetTheme({ theme, id })}>
       {theme ? <LightModeIcon /> : <DarkModeIcon />}

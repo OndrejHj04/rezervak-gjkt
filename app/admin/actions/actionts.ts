25530e2aca34236809cb7108038f56a3f9c67d13
OndrejHj04 <ondra.hajku@seznam.cz>
2024-01-03 15:52:40 +0100

    dark mode 🌃🌉

--- app/admin/actions/actionts.ts
+++ app/admin/actions/actionts.ts
@@ -2,15 +2,10 @@
 import { query } from "@/lib/db";
 
 export const setTheme = async (theme: any, id: any) => {
-  const req = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/api/users/theme`,
-    {
-      method: "POST",
-      body: JSON.stringify({ theme, id }),
-    }
-  );
-  const data = await req.json();
-  return data;
+  const req = (await query({
+    query: `UPDATE users SET theme = ${!theme} WHERE id = "${id}"`,
+    values: [],
+  })) as any;
 };
 
 export const getExportData = async (type: any) => {

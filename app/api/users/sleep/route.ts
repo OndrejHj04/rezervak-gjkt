6dec62225466f29026d4e62b5587ae0794a9c4b9
OndrejHj04 <ondra.hajku@seznam.cz>
2024-01-24 19:20:40 +0100

    sleeping endpoint

--- app/api/users/sleep/route.ts
+++ app/api/users/sleep/route.ts
@@ -1,14 +1,15 @@
 import { query } from "@/lib/db";
+import fetcher from "@/lib/fetcher";
 import protect from "@/lib/protect";
+import { template } from "lodash";
 import { NextResponse } from "next/server";
 
+const eventId = 3;
 export async function POST(req: Request) {
   try {
     const { id, active } = await req.json();
-
-    const isAuthorized = (await protect(
-      req.headers.get("Authorization")
-    )) as any;
+    const token = req.headers.get("Authorization");
+    const isAuthorized = (await protect(token)) as any;
 
     if (!isAuthorized) {
       return NextResponse.json(
@@ -20,9 +21,29 @@ export async function POST(req: Request) {
       );
     }
 
-    const data = await query({
-      query: `UPDATE users SET active = ${!active} WHERE id = "${id}"`,
-      values: [],
+    const [_, user] = (await Promise.all([
+      query({
+        query: `UPDATE users SET active = ? WHERE id = ?`,
+        values: [!active, id],
+      }),
+      query({
+        query: `SELECT email FROM users WHERE id = ?`,
+        values: [id],
+      }),
+    ])) as any;
+
+    const { data } = (await fetcher(`/api/mailing/events/detail/${eventId}`, {
+      token,
+    })) as any;
+
+    await fetcher("/api/email", {
+      method: "POST",
+      token,
+      body: JSON.stringify({
+        to: user[0].email,
+        template: data.template,
+        variables: [{ name: "email", value: user[0].email }],
+      }),
     });
 
     return NextResponse.json({

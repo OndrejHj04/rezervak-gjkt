e95df98dbaaa2444632be66b37e8212fc1c81335
OndrejHj04 <ondra.hajku@seznam.cz>
2024-01-28 19:23:34 +0100

    mailing 📱

--- app/mailing/events/EventsAccordion.tsx
+++ app/mailing/events/EventsAccordion.tsx
@@ -45,89 +45,98 @@ export default function EventsAccordion({
           </AccordionSummary>
 
           <AccordionDetails>
-            {event.children.map((singleEvent: any) => (
-              <ListItem key={singleEvent.id} className="flex justify-between">
-                <div className="flex flex-col">
-                  <ListItemText
-                    primary={singleEvent.primary_txt}
-                    secondary={singleEvent.secondary_txt}
-                  />
-                  <div className="flex gap-2 items-center">
-                    <Typography>Proměnné: </Typography>
-                    {singleEvent.variables.map((item: any, i: any) => (
-                      <Chip key={i} label={varTranslate[item as never]} />
-                    ))}
-                  </div>
-                </div>
-                <div className="flex">
-                  <Controller
-                    {...register(`Checkbox ${singleEvent.id}`)}
-                    control={control}
-                    render={({ field: { value, onChange } }) => {
-                      return (
-                        <FormControlLabel
-                          control={<Switch checked={value} />}
-                          onChange={(e, value) => {
-                            onChange(value);
-                            if (!value) {
-                              watch(`Select ${singleEvent.id}`) &&
-                                setValue(`Select ${singleEvent.id}`, null);
-                              clearErrors(`Select ${singleEvent.id}`);
-                            } else {
-                              !watch(`Select ${singleEvent.id}`) &&
-                                setError(`Select ${singleEvent.id}`, {
-                                  type: "custom",
-                                  message:
-                                    "Aktivní události musí mít přiřazenou šablonu",
-                                });
-                            }
-                          }}
-                          label="Aktivní"
-                        />
-                      );
-                    }}
-                  />
-                  <Controller
-                    {...register(`Select ${singleEvent.id}`)}
-                    control={control}
-                    render={({ field: { value, onChange } }) => {
-                      const error = errors[`Select ${singleEvent.id}`];
-                      return (
-                        <Autocomplete
-                          value={value}
-                          onChange={(_, value) => {
-                            if (errors[`Select ${singleEvent.id}`]) {
-                              clearErrors(`Select ${singleEvent.id}`);
-                            }
-
-                            onChange(value);
-                          }}
-                          options={options}
-                          disabled={!watch(`Checkbox ${singleEvent.id}`)}
-                          renderOption={(props: any, option: any) => (
-                            <div {...props}>
-                              <Typography>{option.name}</Typography>
-                            </div>
-                          )}
-                          getOptionLabel={(option: any) => `${option.name}`}
-                          sx={{ width: 300 }}
-                          renderInput={(params) => (
-                            <TextField
-                              {...params}
-                              label="Šablona"
-                              error={Boolean(error)}
-                              helperText={
-                                Boolean(error) && (error?.message as any)
-                              }
+            <Box sx={{ overflow: "auto" }}>
+              <Box
+                sx={{ width: "100%", display: "table", tableLayout: "fixed" }}
+              >
+                {event.children.map((singleEvent: any) => (
+                  <ListItem
+                    key={singleEvent.id}
+                    className="flex justify-between"
+                  >
+                    <div className="flex flex-col">
+                      <ListItemText
+                        primary={singleEvent.primary_txt}
+                        secondary={singleEvent.secondary_txt}
+                      />
+                      <div className="flex gap-2 items-center">
+                        <Typography>Proměnné: </Typography>
+                        {singleEvent.variables.map((item: any, i: any) => (
+                          <Chip key={i} label={varTranslate[item as never]} />
+                        ))}
+                      </div>
+                    </div>
+                    <div className="flex">
+                      <Controller
+                        {...register(`Checkbox ${singleEvent.id}`)}
+                        control={control}
+                        render={({ field: { value, onChange } }) => {
+                          return (
+                            <FormControlLabel
+                              control={<Switch checked={value} />}
+                              onChange={(e, value) => {
+                                onChange(value);
+                                if (!value) {
+                                  watch(`Select ${singleEvent.id}`) &&
+                                    setValue(`Select ${singleEvent.id}`, null);
+                                  clearErrors(`Select ${singleEvent.id}`);
+                                } else {
+                                  !watch(`Select ${singleEvent.id}`) &&
+                                    setError(`Select ${singleEvent.id}`, {
+                                      type: "custom",
+                                      message:
+                                        "Aktivní události musí mít přiřazenou šablonu",
+                                    });
+                                }
+                              }}
+                              label="Aktivní"
                             />
-                          )}
-                        />
-                      );
-                    }}
-                  />
-                </div>
-              </ListItem>
-            ))}
+                          );
+                        }}
+                      />
+                      <Controller
+                        {...register(`Select ${singleEvent.id}`)}
+                        control={control}
+                        render={({ field: { value, onChange } }) => {
+                          const error = errors[`Select ${singleEvent.id}`];
+                          return (
+                            <Autocomplete
+                              value={value}
+                              onChange={(_, value) => {
+                                if (errors[`Select ${singleEvent.id}`]) {
+                                  clearErrors(`Select ${singleEvent.id}`);
+                                }
+
+                                onChange(value);
+                              }}
+                              options={options}
+                              disabled={!watch(`Checkbox ${singleEvent.id}`)}
+                              renderOption={(props: any, option: any) => (
+                                <div {...props}>
+                                  <Typography>{option.name}</Typography>
+                                </div>
+                              )}
+                              getOptionLabel={(option: any) => `${option.name}`}
+                              sx={{ width: 300 }}
+                              renderInput={(params) => (
+                                <TextField
+                                  {...params}
+                                  label="Šablona"
+                                  error={Boolean(error)}
+                                  helperText={
+                                    Boolean(error) && (error?.message as any)
+                                  }
+                                />
+                              )}
+                            />
+                          );
+                        }}
+                      />
+                    </div>
+                  </ListItem>
+                ))}
+              </Box>
+            </Box>
           </AccordionDetails>
         </Accordion>
       ))}
--- app/mailing/templates/MailingTemplates.tsx
+++ app/mailing/templates/MailingTemplates.tsx
@@ -1,4 +1,5 @@
 import {
+  Box,
   Button,
   Chip,
   Paper,
@@ -17,36 +18,40 @@ import TableListCheckbox from "@/ui-components/TableListCheckbox";
 export default function MailingTemplates({ templates }: { templates: any }) {
   return (
     <Paper className="w-full p-2">
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell sx={{ padding: 1.5 }}>
-              <TemplatesTrash />
-            </TableCell>
-            <TableCell sx={{ padding: 1.5 }}>
-              <Chip label={"Název"} />
-            </TableCell>
-            <TableCell sx={{ padding: 1.5 }}>
-              <Chip label={"Předmět"} />
-            </TableCell>
-            <TableCell sx={{ padding: 1.5 }} />
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {templates.map((temp: any) => (
-            <TableRow key={temp.id}>
-              <TableListCheckbox prop="templates" id={temp.id} />
-              <TableCell sx={{ padding: 1.5 }}>{temp.name}</TableCell>
-              <TableCell sx={{ padding: 1.5 }}>{temp.title}</TableCell>
-              <TableCell sx={{ padding: 1.5 }}>
-                <Link href={`/mailing/templates/detail/${temp.id}`}>
-                  <Button>detail</Button>
-                </Link>
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+      <Box sx={{ overflow: "auto" }}>
+        <Box sx={{ width: "100%", display: "table", tableLayout: "fixed" }}>
+          <Table size="small">
+            <TableHead>
+              <TableRow>
+                <TableCell sx={{ padding: 1.5 }}>
+                  <TemplatesTrash />
+                </TableCell>
+                <TableCell sx={{ padding: 1.5 }}>
+                  <Chip label={"Název"} />
+                </TableCell>
+                <TableCell sx={{ padding: 1.5 }}>
+                  <Chip label={"Předmět"} />
+                </TableCell>
+                <TableCell sx={{ padding: 1.5 }} />
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {templates.map((temp: any) => (
+                <TableRow key={temp.id}>
+                  <TableListCheckbox prop="templates" id={temp.id} />
+                  <TableCell sx={{ padding: 1.5 }}>{temp.name}</TableCell>
+                  <TableCell sx={{ padding: 1.5 }}>{temp.title}</TableCell>
+                  <TableCell sx={{ padding: 1.5 }}>
+                    <Link href={`/mailing/templates/detail/${temp.id}`}>
+                      <Button>detail</Button>
+                    </Link>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </Box>
+      </Box>
     </Paper>
   );
 }
